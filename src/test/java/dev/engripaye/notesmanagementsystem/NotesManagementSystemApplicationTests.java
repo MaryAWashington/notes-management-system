93e7531fcedcdbbc1185fb1b6531ec33cfc800c0
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-09-18 00:38:16 +0100

    code commit by ENGR. IPAYE

---
+++ src/test/java/dev/engripaye/notesmanagementsystem/NotesManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.engripaye.notesmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
